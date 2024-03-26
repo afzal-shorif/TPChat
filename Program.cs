087b2cc9ab6b4e4e36dd29c1601b92b27f663485
afzal18 <asmanik18@gmail.com>
2024-03-26 15:14:57 +0600

    Implement initial UI design for chat window

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+builder.WebHost.UseUrls("https://localhost:6969");
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+app.UseRouting();
+app.MapDefaultControllerRoute();
 
 app.Run();
