ca23de47553cda485c739db8550fd0da75be9b02
afzal18 <asmanik18@gmail.com>
2024-04-27 15:22:19 +0600

    Create xUnit test class library and implement GetNextId class testcase.

---
+++ CimpleChat.Test/GetNextId.cs
@@ -0,0 +1,80 @@
+﻿using CimpleChat.Services;
+using Xunit;
+
+namespace CimpleChat.Test
+{
+    public class TestGetNextId
+    {
+        [Fact]
+        public void TestSingleUserId()
+        {
+            IGetNextId nextId = Helper.GetRequiredService<IGetNextId>();
+            int id = nextId.GetUserId();
+            Assert.False(id <= 0);
+        }
+
+        [Fact]
+        public void TestSingleChannelId()
+        {
+            IGetNextId nextId = Helper.GetRequiredService<IGetNextId>();
+            int id = nextId.GetChannelId();
+            Assert.False(id <= 0);
+        }
+
+        [Fact]
+        public void TestSingleMessageId()
+        {
+            IGetNextId nextId = Helper.GetRequiredService<IGetNextId>();
+            int id = nextId.GetMessageId();
+            Assert.False(id <= 0);
+        }
+
+        [Fact]
+        public void TestMultipleUserId()
+        {
+            IGetNextId nextId = Helper.GetRequiredService<IGetNextId>();
+            List<int> ids = new List<int>();
+            for(int i = 0; i < 255; i++)
+            {
+                int id = nextId.GetUserId();
+                ids.Add(id);
+            }
+
+            List<int> d = ids.Distinct().ToList<int>();
+            
+            Assert.True(ids.Count == d.Count);
+        }
+
+        [Fact]
+        public void TestMultipleChannelId()
+        {
+            IGetNextId nextId = Helper.GetRequiredService<IGetNextId>();
+            List<int> ids = new List<int>();
+            for (int i = 0; i < 255; i++)
+            {
+                int id = nextId.GetChannelId();
+                ids.Add(id);
+            }
+
+            List<int> d = ids.Distinct().ToList<int>();
+
+            Assert.True(ids.Count == d.Count);
+        }
+
+        [Fact]
+        public void TestMultipleMessageId()
+        {
+            IGetNextId nextId = Helper.GetRequiredService<IGetNextId>();
+            List<int> ids = new List<int>();
+            for (int i = 0; i < 255; i++)
+            {
+                int id = nextId.GetMessageId();
+                ids.Add(id);
+            }
+
+            List<int> d = ids.Distinct().ToList<int>();
+
+            Assert.True(ids.Count == d.Count);
+        }
+    }
+}
---
+++ CimpleChat.Test/Helper.cs
@@ -0,0 +1,24 @@
+﻿
+using CimpleChat.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CimpleChat.Test
+{
+    public static class Helper
+    {
+        private static IServiceProvider Provider()
+        {
+            var services = new ServiceCollection();
+            services.AddScoped<IGetNextId,  GetNextId>();
+
+            return services.BuildServiceProvider();
+        }
+
+        public static T GetRequiredService<T>()
+        {
+            var provider = Provider();
+
+            return provider.GetRequiredService<T>();
+        }
+    }
+}
